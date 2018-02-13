98cb49c8a4e67e60f73b856afe5a79f7abc0cfbc
Keenan Brock <keenan@thebrocks.net>
2018-02-13 17:46:25 -0500

    condense ops

--- hrm.rb
+++ hrm.rb
@@ -16,50 +16,16 @@ module HRM
       end
     end
 
-    def outbox(address)
-      puts @value
-      @value = nil
-    end
-
-    def copyfrom(address)
-      @value = @memory[address]
-    end
-
-    def copyto(address)
-      @memory[address] = @value
-    end
-
-    def add(address)
-      @value += @memory[address]
-    end
-
-    def sub(address)
-      @value -= @memory[address]
-    end
-
-    def bumpup(address)
-      @value = @memory[address] += 1
-    end
-
-    def bumpdown(address)
-      @value = @memory[address] -= 1
-    end
-
-    def jump(address)
-      @pc = address
-    end
-
-    def jump_if_zero(address)
-      if @value == 0 || @value == "0"
-        @pc = address
-      end
-    end
-
-    def jump_if_neg(address)
-      if @value < 1 || @value.to_s =~ /^-/
-        @pc = address
-      end
-    end
+    def outbox(address)   ; puts @value ; @value = nil ; end
+    def copyfrom(address) ; @value = @memory[address] ; end
+    def copyto(address)   ; @memory[address] = @value ; end
+    def add(address)      ; @value += @memory[address] ; end
+    def sub(address)      ; @value -= @memory[address] ; end
+    def bumpup(address)   ; @value = @memory[address] += 1 ; end
+    def bumpdown(address) ; @value = @memory[address] -= 1 ; end
+    def jump(address)     ; @pc = address ; end
+    def jump_if_zero(address) ; @pc = address if @value == 0 || @value == "0" ; end
+    def jump_if_neg(address) ; @pc = address  if @value < 1 || @value.to_s =~ /^-/ ; end
   end
 end
 
@@ -89,7 +55,6 @@ module HRM
         @pc += 1
 
         break if instruction.nil?
-
 
         if arg
           if arg =~ /\[\s*(\d+)\s*\]/
